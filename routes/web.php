dfcf4f6c4b600e2f5046c4dce9533bd0cba10019
Snoozey2004 <phamminhhieu192004@gmail.com>
2025-12-30 11:26:01 +0700

    changes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.5');
+});
+Route::get('/2', function () {
+    return view('pages.5');
 });
